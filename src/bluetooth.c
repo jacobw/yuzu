5a94349a95e643a1804ff2c8c7333356a1ae7059
jacobw <jacob@9.nz>
2024-07-09 17:57:42 +1200

    Remove UUIDs from advertising
    Saves 6 bytes. Now 25 bytes.

--- src/bluetooth.c
+++ src/bluetooth.c
@@ -52,10 +52,7 @@ static uint8_t service_data[] = {
 
 static const struct bt_data ad[] = {
     BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-    BT_DATA(BT_DATA_SVC_DATA16, service_data, ARRAY_SIZE(service_data)),
-    BT_DATA_BYTES(BT_DATA_UUID16_ALL,
-                  BT_UUID_16_ENCODE(BT_UUID_DIS_VAL),
-                  BT_UUID_16_ENCODE(BT_UUID_BAS_VAL))};
+    BT_DATA(BT_DATA_SVC_DATA16, service_data, ARRAY_SIZE(service_data))};
 
 /* Declarations */
 
