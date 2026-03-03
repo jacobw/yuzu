b5f71e292d76d19692ab8cec5447c87d45315871
Jacob Winther <jacob@9.nz>
2026-03-03 19:44:03 +1300

    Convert indentation to tabs

--- src/main.c
+++ src/main.c
@@ -20,72 +20,72 @@ const struct device *const sht = SHT_NODE;
 
 int main(void)
 {
-    int err;
-    unsigned int batt_pct = 0;
-    unsigned int batt_mv = 0;
-    unsigned int temp = 0;
-    unsigned int hum = 0;
-    struct sensor_value temp_raw, hum_raw;
+	int err;
+	unsigned int batt_pct = 0;
+	unsigned int batt_mv = 0;
+	unsigned int temp = 0;
+	unsigned int hum = 0;
+	struct sensor_value temp_raw, hum_raw;
 
 	if (!gpio_is_ready_dt(&led)) {
 		LOG_ERR("LED GPIO device not ready");
 		return -ENODEV;
 	}
 
-    if (!device_is_ready(sht)) {
-        LOG_ERR("SHT device %s is not ready", sht->name);
-    } else {
-        LOG_INF("SHT Device ready");
-    }
+	if (!device_is_ready(sht)) {
+		LOG_ERR("SHT device %s is not ready", sht->name);
+	} else {
+		LOG_INF("SHT Device ready");
+	}
 
-    err = bluetooth_init();
-    if (err != 0) {
-        LOG_ERR("Failed to update advertising data (err %d)", err);
-        return 0;
-    }
+	err = bluetooth_init();
+	if (err != 0) {
+		LOG_ERR("Failed to update advertising data (err %d)", err);
+		return 0;
+	}
 
-    err = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+	err = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
 	if (err < 0) {
 		LOG_ERR("Failed to configure LED GPIO (err %d)", err);
 		return err;
 	}
 
-    while (true) {
-        // Get battery data
-        batt_mv = battery_sample();
-        if (batt_mv < 0) {
-            LOG_WRN("Failed to read battery voltage: %d", batt_mv);
-            continue;
-        }
-        batt_pct = battery_level_pptt(batt_mv, discharge_curve_cr2032);
+	while (true) {
+		// Get battery data
+		batt_mv = battery_sample();
+		if (batt_mv < 0) {
+			LOG_WRN("Failed to read battery voltage: %d", batt_mv);
+			continue;
+		}
+		batt_pct = battery_level_pptt(batt_mv, discharge_curve_cr2032);
 
-        // Get tempertaure and humidity data
-        if (sht) {
-            err = sensor_sample_fetch(sht);
-            if (err == 0) {
-                err = sensor_channel_get(sht, SENSOR_CHAN_AMBIENT_TEMP, &temp_raw);
-            }
-            if (err == 0) {
-                err = sensor_channel_get(sht, SENSOR_CHAN_HUMIDITY, &hum_raw);
-            }
-            if (err != 0) {
-                LOG_WRN("SHT failed: %d", err);
-                continue;
-            }
-            temp = roundf(sensor_value_to_float(&temp_raw) * 10) * 10;
-            hum = roundf(sensor_value_to_float(&hum_raw) * 10) * 10;
-        }
+		// Get tempertaure and humidity data
+		if (sht) {
+			err = sensor_sample_fetch(sht);
+			if (err == 0) {
+				err = sensor_channel_get(sht, SENSOR_CHAN_AMBIENT_TEMP, &temp_raw);
+			}
+			if (err == 0) {
+				err = sensor_channel_get(sht, SENSOR_CHAN_HUMIDITY, &hum_raw);
+			}
+			if (err != 0) {
+				LOG_WRN("SHT failed: %d", err);
+				continue;
+			}
+			temp = roundf(sensor_value_to_float(&temp_raw) * 10) * 10;
+			hum = roundf(sensor_value_to_float(&hum_raw) * 10) * 10;
+		}
 
-        LOG_INF("Batt %d%% %dmV, Temp: %dC, Hum %d%%RH", batt_pct / 100, batt_mv, temp, hum);
-        bluetooth_update(batt_pct, batt_mv, temp, hum);
+		LOG_INF("Batt %d%% %dmV, Temp: %dC, Hum %d%%RH", batt_pct / 100, batt_mv, temp, hum);
+		bluetooth_update(batt_pct, batt_mv, temp, hum);
 
-        /* Flash LED after successful update */
-        gpio_pin_set_dt(&led, 1);
-        k_msleep(10);
-        gpio_pin_set_dt(&led, 0);
+		/* Flash LED after successful update */
+		gpio_pin_set_dt(&led, 1);
+		k_msleep(10);
+		gpio_pin_set_dt(&led, 0);
 
-        k_msleep(CONFIG_YUZU_SAMPLE_INTERVAL_MS);
-    };
+		k_msleep(CONFIG_YUZU_SAMPLE_INTERVAL_MS);
+	};
 
-    return 0;
+	return 0;
 }
